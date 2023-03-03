3114091f1fb5d37ffed4d4e371a7b86170355946
ThatHomr <116254847+ThatHomr@users.noreply.github.com>
2023-03-03 12:55:40 +0900

    23-03-03 13시 크롤링 selenium

---
+++ 05_crawling/15_selenium_test02.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.naver.com')
+search = driver.find_element(By.ID, 'query')
+search.send_keys('시바견')
+search.send_keys(Keys.ENTER)
+time.sleep(5)
+item = driver.find_elements(By.CSS_SELECTOR, 'mark')
+item[0].click()
+time.sleep(5)
+page = driver.page_source
+print(page)
+time.sleep(5)
+driver.quit()
