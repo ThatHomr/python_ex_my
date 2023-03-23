4893e4db91395c0621f3595154513b0d970268f2
ThatHomr <116254847+ThatHomr@users.noreply.github.com>
2023-03-23 13:59:59 +0900

    23-03-23

--- 05_crawling/01_download_img.py
+++ 05_crawling/01_download_img.py
@@ -1,6 +1,7 @@
 from urllib import request
 
 url = 'https://pds.joongang.co.kr/news/FbMetaImage/202212/0e5449f1-442b-441e-b623-5e8a9eacf6ab.jpg'
+
 filename = '05_crawling/down/siba.jpg'
 
 request.urlretrieve(url, filename)
